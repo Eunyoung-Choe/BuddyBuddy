e3e404a94a20d9a7e4f7cbe9397d42a992ff63a0
Eunyoung-Choe <ceytown@student.hywoman.ac.kr>
2025-03-05 15:28:36 +0900

    first commit

---
+++ src/main/java/com/example/demo/SpringBuddyMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBuddyMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBuddyMvcApplication.class, args);
+	}
+
+}
